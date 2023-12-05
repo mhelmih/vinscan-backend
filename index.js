585c4b363a9760459eeec2594a8baaea5ea88390
Volem Alvaro Azira <volemalvaroazira2@gmail.com>
2023-12-05 15:44:37 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
